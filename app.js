0cf174c560aeea636ac629947810679c6274569f
Hubbister <serventi.lorenzo@gmail.com>
2018-09-09 22:23:56 +0200

    challenge 6 completed

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 */
 
-var scores, roundScore, activePlayer, gamePlaying, lastroll, winScore;
+var scores, roundScore, activePlayer, gamePlaying, winScore;
 
 init();
 
@@ -15,36 +15,38 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
     
     if (gamePlaying) {
 
-    // 1. Random number
-        var dice = Math.floor(Math.random() * 6) + 1;
-    
-    // 2. Display the results
-        var diceDOM = document.querySelector('.dice');
-        diceDOM.style.display = 'block';
-        diceDOM.src = 'dice-' +  dice + '.png';
+// 1. Random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+    var dice1 = Math.floor(Math.random() * 6) + 1;
+
+// 2. Display the results
+    var diceDOM = document.querySelector('.dice');
+    var diceDOM1 = document.querySelector('.dice1');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' +  dice + '.png';
+    diceDOM1.style.display = 'block';
+    diceDOM1.src = 'dice-' +  dice1 + '.png';
+/*
     // se ha fatto due 6 di fila perde TUTTO
         if (lastroll === 6 && lastroll === dice) {
             scores[activePlayer] = 0;
             roundScore = 0;
             changeActive();
-        }
+        }*/
     // 3. update the round score if the rolled number is not a 1
-        else if (dice !== 1 ){
-            lastroll = dice;
-        //add score
-            roundScore += dice;
-            document.querySelector('#current-' + activePlayer).textContent = roundScore;
-    
-        } else {
-       
+        if (dice1 === 1 || dice === 1){
+            console.log(dice1, dice);
         //next player
-        
             changeActive();
-        
-//        }
-    
         }
     
+        else {
+        //    lastroll = dice;
+        //add score
+            roundScore += dice + dice1;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+            
+        } 
     }
 
 
@@ -59,9 +61,8 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
 // Update the ui
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 // Check if player won the game
-
         winScore = document.querySelector('#inputValue').value;
-        console.log(winScore);
+        
         if (scores[activePlayer] >= winScore) {
             winner();
         } else {
@@ -79,6 +80,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
 function winner() {
     document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
     document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.dice1').style.display = 'none';
     document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
     document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');            
     document.querySelector('.btn-roll').style.display = 'none';
@@ -93,8 +95,9 @@ function changeActive() {
     document.querySelector('.player-0-panel').classList.toggle('active');
     document.querySelector('.player-1-panel').classList.toggle('active');
     document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.dice1').style.display = 'none';
     roundScore = 0;
-    lastroll = 0;
+    //lastroll = 0;
 }
 
 
@@ -106,9 +109,10 @@ function init() {
     activePlayer = 0;
     roundScore = 0;
     gamePlaying = true;
-    lastroll = 0;
+    //lastroll = 0;
 
     document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.dice1').style.display = 'none';
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
     document.getElementById('current-0').textContent = '0';
