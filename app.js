bd57e800d1204091910ffef90b1c19c098bd6607
Hubbister <serventi.lorenzo@gmail.com>
2018-09-09 20:33:18 +0200

    pig hold

--- app.js
+++ app.js
@@ -72,8 +72,12 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
         //document.getElementById('.current-1').textContent = scores[activePlayer];
     // Check if player won the game
-        if (scores[activePlayer] >= 100) {
-            document.querySelector('#name-' + activePlayer).textContent = 'winner';
+        if (scores[activePlayer] >= 20) {
+            document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+            
         } else {
             //next player
             changeActive();
